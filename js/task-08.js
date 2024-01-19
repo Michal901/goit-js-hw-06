d1ca3abe409d06bbb0bd55282c14ebbbdba0192c
Michal901 <momot1122@wp.pl>
2024-01-19 15:15:21 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  //   const formData = {};
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("All fields must be filled in!");
+  }
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}

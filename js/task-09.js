42d354ee5b30433ac1db6b4c9036ba244b7d3d60
Michal901 <momot1122@wp.pl>
2024-01-20 15:26:57 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(".widget");
+const colorGenerator = widget.querySelector(".change-color");
+const color = widget.querySelector(".color");
+
+function changeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+}
+
+colorGenerator.addEventListener("click", changeColor);
